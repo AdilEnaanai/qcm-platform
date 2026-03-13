9ec3d2614429d02cb309b62fa75a5e13c06a2007
ENAANAI <aenaanai@uae.ac.ma>
2026-03-13 18:03:26 +0000

    second commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-
+//C'est tout
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
